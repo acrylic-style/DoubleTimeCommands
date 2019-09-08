8593a0a7364ee52e3242e3160e826897328caddd
acrylic-style <me@acrylicstyle.xyz>
2019-09-08 22:46:41 +0900

    test

--- src/main/java/xyz/acrylicstyle/doubletimecommands/events/PlayerCommandPreprocess.java
+++ src/main/java/xyz/acrylicstyle/doubletimecommands/events/PlayerCommandPreprocess.java
@@ -8,6 +8,7 @@ import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.EventExecutor;
 import xyz.acrylicstyle.doubletimecommands.DoubleTimeCommands;
 import xyz.acrylicstyle.doubletimecommands.utils.PlayerUtils;
+import xyz.acrylicstyle.tomeito_core.utils.Log;
 import xyz.acrylicstyle.tomeito_core.utils.Ranks;
 
 import java.util.regex.Pattern;
@@ -15,6 +16,7 @@ import java.util.regex.Pattern;
 public class PlayerCommandPreprocess implements EventExecutor {
     @Override
     public void execute(Listener listener, Event e) throws EventException {
+        Log.debug("called");
         PlayerCommandPreprocessEvent event = (PlayerCommandPreprocessEvent) e;
         if (DoubleTimeCommands.bungee == null) return;
         String[] args = event.getMessage().replaceAll(Pattern.quote("/"), "").split(" ");
