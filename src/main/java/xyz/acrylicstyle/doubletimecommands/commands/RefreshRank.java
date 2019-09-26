583e83755a90279655891be49cfc55600b2e0271
acrylic-style <me@acrylicstyle.xyz>
2019-09-26 17:52:56 +0900

    use atomic, array is 0 iq

--- src/main/java/xyz/acrylicstyle/doubletimecommands/commands/RefreshRank.java
+++ src/main/java/xyz/acrylicstyle/doubletimecommands/commands/RefreshRank.java
@@ -10,6 +10,8 @@ import xyz.acrylicstyle.doubletimecommands.utils.PlayerUtils;
 import xyz.acrylicstyle.tomeito_core.utils.Log;
 import xyz.acrylicstyle.tomeito_core.utils.Ranks;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class RefreshRank implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -17,25 +19,26 @@ public class RefreshRank implements CommandExecutor {
             sender.sendMessage(ChatColor.RED + "This command should send from in-game.");
             return true;
         }
-        final Player[] player = {(Player) sender};
-        Log.debug("UUID: " + player[0].getUniqueId());
-        Log.debug("Player name: " + player[0].getName() + ", Sender name: " + sender.getName());
+        final AtomicReference<Player> player = new AtomicReference<>();
+        player.set((Player) sender);
+        Log.debug("UUID: " + player.get().getUniqueId());
+        Log.debug("Player name: " + player.get().getName() + ", Sender name: " + sender.getName());
         try {
-            Ranks before = PlayerUtils.getRank(player[0].getUniqueId());
-            PlayerUtils.refreshRank(player[0], new Callback<Ranks>() {
+            Ranks before = PlayerUtils.getRank(player.get().getUniqueId());
+            PlayerUtils.refreshRank(player.get(), new Callback<Ranks>() {
                 @Override
                 public void done(Ranks after, Throwable e) {
-                    Log.debug("Result for " + player[0].getUniqueId() + ": " + after.name());
-                    String name = PlayerUtils.getName(player[0], after);
-                    player[0].setDisplayName(name);
-                    player[0].setPlayerListName(name);
+                    Log.debug("Result for " + player.get().getUniqueId() + ": " + after.name());
+                    String name = PlayerUtils.getName(player.get(), after);
+                    player.get().setDisplayName(name);
+                    player.get().setPlayerListName(name);
                     if (before.equals(after)) {
                         Log.debug("No updates.");
-                        player[0].sendMessage(ChatColor.GREEN + "Refreshed rank, but you're still " + before.name() + " because we couldn't find any changes.");
+                        player.get().sendMessage(ChatColor.GREEN + "Refreshed rank, but you're still " + before.name() + " because we couldn't find any changes.");
                         return;
                     }
                     Log.debug("New rank: " + after.name() + " from " + before.name());
-                    player[0].sendMessage(ChatColor.GREEN + "Refreshed rank, new your rank is " + after.name() + "! Enjoy!");
+                    player.get().sendMessage(ChatColor.GREEN + "Refreshed rank, new your rank is " + after.name() + "! Enjoy!");
                 }
             });
         } catch (NullPointerException e) {
