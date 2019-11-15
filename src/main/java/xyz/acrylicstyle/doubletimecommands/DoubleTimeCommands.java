744f5acd232e0b591fb3194c9ded3f4816737b12
acrylic-style <me@acrylicstyle.xyz>
2019-11-15 22:46:38 +0900

    highest?

--- src/main/java/xyz/acrylicstyle/doubletimecommands/DoubleTimeCommands.java
+++ src/main/java/xyz/acrylicstyle/doubletimecommands/DoubleTimeCommands.java
@@ -197,7 +197,7 @@ public class DoubleTimeCommands extends JavaPlugin implements Listener {
 
     private CollectionList<UUID> punches = new CollectionList<>();
 
-    @EventHandler(priority = EventPriority.HIGH)
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onEntityDamageByEntity(EntityDamageByEntityEvent e) {
         Log.info("fired");
         e.setCancelled(true);
